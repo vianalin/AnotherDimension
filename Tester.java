ab0a6f8abc481b59008eb71d7780c8fa64c6903c
vianalin <vlin21@stuy.edu>
2020-10-25 01:13:55 -0400

    testing sumrows but array id. going to add demo.java

--- Tester.java
+++ Tester.java
@@ -4,6 +4,6 @@ public class Tester {
     public static void main(String[] args) {
     	System.out.println(ArrayOps.sum(new int[] {1, 3, 4})); //4a
     	System.out.println(ArrayOps.largest(new int[] {1, 2, 9, 1, 7})); //4b
-
+    	System.out.println(ArrayOps.sumRows(new int[][] {{1}, {2,3}})); // 4c
     }
 }
