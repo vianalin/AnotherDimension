3f556f688a9535b040a6babe2466daceffb8c5ad
vianalin <vlin21@stuy.edu>
2020-10-25 00:30:15 -0400

    tested largest

--- Tester.java
+++ Tester.java
@@ -2,6 +2,8 @@ public class Tester {
 	public static ArrayOps arrayOps = new ArrayOps();
 
     public static void main(String[] args) {
-    	System.out.println(ArrayOps.sum(new int[] {1, 3, 4}));
+    	System.out.println(ArrayOps.sum(new int[] {1, 3, 4})); //4a
+    	System.out.println(ArrayOps.largest(new int[] {1, 2, 9, 1, 7})); //4b
+
     }
 }
