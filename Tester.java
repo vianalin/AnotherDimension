00cf4bbb4bb7f7e01fa3517dba4cda9def373438
vianalin <vlin21@stuy.edu>
2020-10-25 15:29:56 -0400

    tested sumrows

--- Tester.java
+++ Tester.java
@@ -1,9 +1,10 @@
 public class Tester { 
 	public static ArrayOps arrayOps = new ArrayOps();
+	public static Demo demo = new Demo();
 
     public static void main(String[] args) {
     	System.out.println(ArrayOps.sum(new int[] {1, 3, 4})); //4a
     	System.out.println(ArrayOps.largest(new int[] {1, 2, 9, 1, 7})); //4b
-    	System.out.println(ArrayOps.sumRows(new int[][] {{1}, {2,3}})); // 4c
+    	System.out.println(Demo.arrToString(ArrayOps.sumRows(new int[][] {{1}, {2,3}}))); // 4c
     }
 }
