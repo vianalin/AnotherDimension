9304e68045b91d33b14a2bde1aa6aae3b603288b
vianalin <vlin21@stuy.edu>
2020-10-25 18:23:22 -0400

    tested largestinrows

--- Tester.java
+++ Tester.java
@@ -6,5 +6,6 @@ public class Tester {
     	System.out.println(ArrayOps.sum(new int[] {1, 3, 4})); //4a
     	System.out.println(ArrayOps.largest(new int[] {1, 2, 9, 1, 7})); //4b
     	System.out.println(Demo.arrToString(ArrayOps.sumRows(new int[][] {{1}, {2,3}}))); // 4c
+    	System.out.println(Demo.arrToString(ArrayOps.largestInRows(new int[][] {{1}, {2,3}}))); //4d
     }
 }
