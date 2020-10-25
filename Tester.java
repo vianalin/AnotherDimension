934f0973290be02e5f6351598f7b1d78430c4c7e
vianalin <vlin21@stuy.edu>
2020-10-25 00:24:13 -0400

    tested sum

--- Tester.java
+++ Tester.java
@@ -1,5 +1,7 @@
 public class Tester { 
-    public static void main(String[] args) {
+	public static ArrayOps arrayOps = new ArrayOps();
 
+    public static void main(String[] args) {
+    	System.out.println(ArrayOps.sum(new int[] {1, 3, 4}));
     }
 }
