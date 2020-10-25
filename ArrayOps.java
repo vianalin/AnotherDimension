0973d5a71beae0c686cd56561cc48487ade462cb
vianalin <vlin21@stuy.edu>
2020-10-25 01:14:38 -0400

    4c not done yet

--- ArrayOps.java
+++ ArrayOps.java
@@ -15,5 +15,17 @@ public class ArrayOps {
 		return max;
 	}
 
+	public static int[] sumRows(int[][] matrix) {
+		int sum = 0;
+		int result[] = new int[matrix.length];
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[i].length; j++) {
+				sum = matrix[i][j];
+				result[i] += sum;
+			}
+		}
+		return result;
+	}
+
 
 }
