e57d49ad60811e77513ff44bcb40b698ae1db348
vianalin <vlin21@stuy.edu>
2020-10-25 00:30:47 -0400

    4b largest

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,9 +1,19 @@
 public class ArrayOps {
-	public static int sum(int[] arr) {
+	public static int sum(int[] arr) { //4a
 		int sum = 0;
 		for(int i = 0; i < arr.length; i++) {
 			sum += arr[i];
 		}
 		return sum;
 	}
+
+	public static int largest(int[] arr) {
+		int max = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] > max) max = arr[i];
+		}
+		return max;
+	}
+
+
 }
